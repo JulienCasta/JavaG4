be4844da0d6c07add07f24ab20c387b11e2c9d8a
julien <juliencasta16@gmail.com>
2021-04-26 10:38:15 +0200

    add

---
+++ src/Point.java
@@ -0,0 +1,22 @@
+public class Point {
+    public final static float DFL_X=0;
+    public final static float DFL_Y=0;
+    private float x, y;
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    Point
+}
