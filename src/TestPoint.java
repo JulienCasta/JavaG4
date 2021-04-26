a4aa9a3bc026cfa4b77c5efb75cacdb16edf3fda
Walczak Mathieu <mathwal@hotmail.fr>
2021-04-26 11:18:10 +0200

    point.move

--- src/TestPoint.java
+++ src/TestPoint.java
@@ -1,5 +1,12 @@
 public class TestPoint {
     public static void main(String[] args) {
-        Point p = New Point();
+        Point p = new Point(4,5);
+        Point p1 = new Point();
+        Point p2 = new Point();
+
+        System.out.println(p);
+        System.out.println(p1);
+        System.out.println(p2);
+
     }
 }
